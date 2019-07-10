0207aa429dd4c7fcd0f4399ed1ab692c2c8892e1
johnboland91 <johnboland91@gmail.com>
2019-07-10 16:23:53 -0500

    Creates articles views and forms

--- activism/articles/views.py
+++ activism/articles/views.py
@@ -1,3 +1,60 @@
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView, ListView, UpdateView, DetailView
+from django.urls import reverse
 from django.shortcuts import render
 
-# Create your views here.
+from activism.helpers import AuthorRequiredMixin
+from .models import Article
+
+class ArticlesListView(LoginRequiredMixin, ListView):
+    """Basic ListView implementation to call the published articles list."""
+    model = Article
+    paginate_by = 15
+    context_object_name = "articles"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['popular_tags'] = Article.objects.get_counted_tags()
+        return context
+
+    def get_queryset(self, **kwargs):
+        return Article.objects.get_published()
+
+class DraftsListView(ArticlesListView):
+    """Overriding the original implementation to call the drafts articles list."""
+
+    def get_queryset(self, **kwargs):
+        return Article.objects.get_drafts(
+
+class CreateArticleView(LoginRequiredMixin, CreateView):
+    """Basic ListView implementation to call the published articles list."""
+    model = Article
+    message = "Your article has been created."
+    form_class = ArticleForm
+    template_name = "articles/article_create.html"
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('articles:list')
+
+class EditArticleView(LoginRequiredMixin, AuthorRequiredMixin, UpdateView):
+    """Basic EditView implementation to edit existing articles."""
+    model = Article
+    message = ("Your article has been updated")
+    form_class = ArticleForm
+    template_name = "articles/article_edit.html"
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('articles:list')
+
+class DetailArticleView(LoginRequiredMixin, DetailView):
+    """Basic DetailView implementation to call an individual article."""
+    model = Article 
